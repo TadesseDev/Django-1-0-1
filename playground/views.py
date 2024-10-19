e0a3177df1890e59529390eaef3dde93bc90d98c
tadessedev <tadessedev@gmail.com>
2024-10-20 01:20:54 +0300

    feat: render the initial views

--- playground/views.py
+++ playground/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def root(request):
+  return HttpResponse("Root app")
+def say_hello(request):
+  return HttpResponse("hello world form the")
